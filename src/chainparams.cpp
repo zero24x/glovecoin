22ec3eefec074b8868b4bdcb3bc1c05570ffdb97
zero24x <zero@you-spam.com>
2019-04-22 03:04:37 +0200

    pow end

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -107,7 +107,7 @@ public:
 
         convertSeed6(vFixedSeeds, pnSeed6_main, ARRAYLEN(pnSeed6_main));
 
-        nLastPOWBlock = 43200;
+        nLastPOWBlock = 1000;
     }
 
     virtual const CBlock& GenesisBlock() const { return genesis; }
